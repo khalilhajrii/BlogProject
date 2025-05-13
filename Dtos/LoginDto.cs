ad0d6a15dd20ec783b552edde176501a73ee9f86
khalilhajrii <khalil.hajri@esen.tn>
2025-05-13 20:46:28 +0100

    ADD Dto shared project

---
+++ Dtos/LoginDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dtos
+{
+    public class LoginDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [MinLength(6, ErrorMessage = "Password has to be at least 6 characters")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
