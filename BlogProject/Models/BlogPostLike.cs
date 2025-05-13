33bcf34c4ae3bcd25ba1a842819d7fc24653d4f4
khalilhajrii <khalil.hajri@esen.tn>
2025-05-13 18:31:16 +0100

    initialize project + add modals + install required dependecies

---
+++ BlogProject/Models/BlogPostLike.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogProject.Models
+{
+    public class BlogPostLike
+    {
+        public Guid Id { get; set; }
+        public Guid BlogPostId { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
---
+++ BlogProject/Program.cs
@@ -0,0 +1,29 @@
+using Bloggie.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BloggieDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("BloggieDbConnectionString")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
